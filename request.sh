f70aad4b165ad5ec81695b12947309e909ba7020
ehabhamdy <ehabhamdy2012@gmail.com>
2025-11-30 19:14:23 +0100

    update request to use deployed api

--- request.sh
+++ request.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Request the filtered image from the local server
+
 curl -H "X-API-Key: udacity-image-filter-2025" \
-"http://localhost:8082/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg" \
+"http://image-filter-app-dev22.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg" \
 --output filtered_image.jpg
